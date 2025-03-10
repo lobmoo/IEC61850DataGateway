bccf821d7b11901412262896963316e9d33f216d
lobmo <1162431386@qq.com>
2025-03-10 10:16:09 +0800

    add:添加example，方便后续的使用

---
+++ sample/node_example/CMakeFiles/node_example_exec.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wwk/workspaces/test_demo/sample/node_example/HelloWorldOne/source/HelloWorldOnePubSubTypes.cxx" "sample/node_example/CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOnePubSubTypes.cxx.o" "gcc" "sample/node_example/CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOnePubSubTypes.cxx.o.d"
+  "/home/wwk/workspaces/test_demo/sample/node_example/HelloWorldOne/source/HelloWorldOneTypeObjectSupport.cxx" "sample/node_example/CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOneTypeObjectSupport.cxx.o" "gcc" "sample/node_example/CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOneTypeObjectSupport.cxx.o.d"
+  "/home/wwk/workspaces/test_demo/common/fastdds_wrapper/DDSDomainParticipant.cpp" "sample/node_example/CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSDomainParticipant.cpp.o" "gcc" "sample/node_example/CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSDomainParticipant.cpp.o.d"
+  "/home/wwk/workspaces/test_demo/common/fastdds_wrapper/DDSParticipantManager.cpp" "sample/node_example/CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSParticipantManager.cpp.o" "gcc" "sample/node_example/CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSParticipantManager.cpp.o.d"
+  "/home/wwk/workspaces/test_demo/common/fastdds_wrapper/ParticipantQosHandler.cpp" "sample/node_example/CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/ParticipantQosHandler.cpp.o" "gcc" "sample/node_example/CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/ParticipantQosHandler.cpp.o.d"
+  "/home/wwk/workspaces/test_demo/sample/node_example/example.cpp" "sample/node_example/CMakeFiles/node_example_exec.dir/example.cpp.o" "gcc" "sample/node_example/CMakeFiles/node_example_exec.dir/example.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sample/node_example/CMakeFiles/node_example_exec.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "../../node_example_exec"
+  "../../node_example_exec.pdb"
+  "CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOnePubSubTypes.cxx.o"
+  "CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOnePubSubTypes.cxx.o.d"
+  "CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOneTypeObjectSupport.cxx.o"
+  "CMakeFiles/node_example_exec.dir/HelloWorldOne/source/HelloWorldOneTypeObjectSupport.cxx.o.d"
+  "CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSDomainParticipant.cpp.o"
+  "CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSDomainParticipant.cpp.o.d"
+  "CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSParticipantManager.cpp.o"
+  "CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/DDSParticipantManager.cpp.o.d"
+  "CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/ParticipantQosHandler.cpp.o"
+  "CMakeFiles/node_example_exec.dir/__/__/common/fastdds_wrapper/ParticipantQosHandler.cpp.o.d"
+  "CMakeFiles/node_example_exec.dir/example.cpp.o"
+  "CMakeFiles/node_example_exec.dir/example.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_example_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
