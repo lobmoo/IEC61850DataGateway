0e7aa628cd7e448f9731de3c933ad9a550ebd057
lobmo <1162431386@qq.com>
2025-04-27 18:31:59 +0800

    fix: 搞了点花活

--- core/main.cc
+++ core/main.cc
@@ -9,19 +9,38 @@
 #include "config/config.h"
 #include "redis-api/app_redis.h"
 
+std::string girl = R"(
+                +++++++IEC61850+++++++
+                       .::::.
+                     .::::::::.
+                    :::::::::::
+                 ..::::::::::'
+              '::::::::::::'
+                .::::::::::
+           '::::::::::::::..
+                ..::::::::::::.
+              ``::::::::::::::::
+               ::::``:::::::::'        .:::.
+              ::::'   ':::::'       .::::::::.
+            .::::'      ::::     .:::::::'::::.
+           .:::'       :::::  .:::::::::' ':::::.
+          .::'        :::::.:::::::::'      ':::::.
+         .::'         ::::::::::::::'         ``::::.
+     ...:::           ::::::::::::'              ``::.
+    ````':.          ':::::::::'                  ::::..
+                       '.:::::'                    ':'````..
+    )";
+
 int main()
 {
     Logger::Instance().setFlushOnLevel(Logger::info);
     Logger::Instance().Init("log/myapp.log", Logger::both, Logger::trace, 60, 5);
+    std::cout << girl << std::endl;
     auto &ptr = Config::getInstance();
     ptr.init("config");
     AppModbus appModbus;
     appModbus.run();
-    while(1);
+    while (1)
+        ;
     return 0;
 }
-
-
-
-
-
